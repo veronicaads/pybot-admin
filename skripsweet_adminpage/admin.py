f5f3bea2dc420f23b6c825978686de1c80b16d91
risk1996 <williamdariansutedjo@gmail.com>
2019-04-01 22:16:16 +0200

    Admin Page Registration

--- skripsweet_adminpage/admin.py
+++ skripsweet_adminpage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from skripsweet_adminpage.models import Admin, Produk, Logistik, Bank, TransactionHeader, OrderDetails, BuktiTransfer, InfoShop, Chat, Chatroom, ChatHistory
 
-# Register your models here.
+admin.register(Admin)
+admin.register(Produk)
+admin.register(Logistik)
+admin.register(Bank)
+admin.register(TransactionHeader)
+admin.register(OrderDetails)
+admin.register(BuktiTransfer)
+admin.register(InfoShop)
+admin.register(Chat)
+admin.register(Chatroom)
+admin.register(ChatHistory)
