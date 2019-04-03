406a896a70fa80ad7d1df50027475e3e4d6829e0
risk1996 <williamdariansutedjo@gmail.com>
2019-04-03 23:47:27 +0200

    Update form submission

--- skripsweet_adminpage/views.py
+++ skripsweet_adminpage/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Count
 from .models import *
 
 def index(request):
@@ -7,6 +8,33 @@ def index(request):
 
 def process_form(request):
     data = request.POST.copy()
-    # th = TransactionHeader()
+    th = TransactionHeader()
+    th.idtransaction = str(int(TransactionHeader.objects.annotate(Count('idtransaction'))) + 1)
+    th.nohp = data.get('telp')
+    th.namacust = data.get('nama')
+    id = data.get('id[]')
+    kuantitas = data.get('kuantitas[]')
+    gambar = data.get('gambar[]')
+    keterangan = data.get('keterangan[]')
+    totalharga = 0
+    for i in range(len(id)):
+        od = OrderDetails()
+        od.idorder = str(int(OrderDetails.objects.annotate(Count('idorder'))) + 1)
+        od.idproduk = Produk.objects.find(pk=id[i])
+        od.qty = int(kuantitas[i])
+        od.gambar = gambar[i]
+        od.keterangan = keterangan[i]
+        od.save()
+        totalharga += od.idproduk.harga * od.qty
+    th.totalharga = 0
+    th.alamatkirim = data.get('alamat')
+    th.noresi = None
+    th.ongkoskirim = 20000
+    th.keterangantambahan = "-"
+    th.idadmin = Admin.objects.first()
+    th.idlogistik = Logistik.objects.first()
+    th.save()
+    th.totalharga = totalharga
+    th.save()
     return HttpResponse(str(data))
 
